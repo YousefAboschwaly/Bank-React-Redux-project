4c09674c8532b0c1ae76d45468d58df37e0eb6ed
yousef <yousefshwaly@gmail.com>
2025-06-30 18:21:46 +0300

    Install Redux and use it in old way using createStore

--- src/Store.js
+++ src/Store.js
@@ -1,25 +1,49 @@
+import { createStore } from "redux";
+const initialState = {
+  balance: 0,
+  loan: 0,
+  loanPurpose: "",
+};
 
-const initialState ={
-  balance:0,
-  loan:0,
-  loanPurpose:""
-}
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "account/deposit":
+      return { ...state, balance: state.balance + action.payload };
+    case "account/withdraw":
+      return { ...state, balance: state.balance - action.payload };
+    case "account/requestLoan": {
+      if (state.loan > 0) return;
+      return {
+        ...state,
+        loan: action.payload.amount,
+        balance: state.balance + action.payload.amount,
+        loanPurpose: action.payload.purpose,
+      };
+    }
 
-function reducer(state=initialState,action){
-switch(action.type){
-  case "account/deposit":return{...state, balance:state.balance+action.payload}
-  case "account/withdraw": return{...state , balance:state.balance-action.payload}
-  case "account/requestLoan":{
-    if(state.loan>0) return
-    return{...state , loan:state.loan+action.payload}
+    case "account/payLoan":
+      return {
+        ...state,
+        loanPurpose: action.payload,
+        loan: state.balance - state.loan,
+      };
+    default:
+      return state;
   }
-
-  case "account/payLoan":return{...state , loanPurpose:action.payload , loan: state.balance-state.loan}
-  default:return state
-
-
-
-
-
 }
-}
+
+const store = createStore(reducer)
+
+
+store.dispatch({ type: "account/deposit", payload: 500 })
+console.log(store.getState());
+
+store.dispatch({ type: "account/withdraw", payload: 200 })
+console.log(store.getState());
+
+store.dispatch({
+  type: "account/requestLoan",
+  payload: { amount: 1000, purpose: "buy a car" },
+});
+console.log(store.getState());
+
