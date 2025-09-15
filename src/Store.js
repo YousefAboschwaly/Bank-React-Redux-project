e1380e134da297efb940a0eba55ea6867badafeb
yousef <yousefshwaly@gmail.com>
2025-09-15 15:30:14 +0300

    Install Redux-dev-tools and use it in Store

--- src/Store.js
+++ src/Store.js
@@ -1,8 +1,8 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
+import { thunk } from 'redux-thunk'
 import customerReducer from "./features/customers/customerSlice";
 import accountReducer from "./features/accounts/accountSlice";
-
-import { thunk } from 'redux-thunk'
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 
 
@@ -11,7 +11,12 @@ const finalReducer = combineReducers({
   customer: customerReducer,
 })
 
-export const store = createStore(finalReducer,applyMiddleware(thunk));
+//  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// export const store = createStore(finalReducer, composeEnhancers( applyMiddleware(thunk)));
+
+
+export const store = createStore(finalReducer, composeWithDevTools( applyMiddleware(thunk)));
 
 
 
